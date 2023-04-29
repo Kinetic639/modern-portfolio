46da99b327456ae0b146903bf4387b8ed7119459
Kinetic639 <michal.stepien36@gmail.com>
2023-04-29 14:42:51 +0200

    Implemented Header section

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Navbar } from "./components/Navbar/Navbar";
 
 import "./App.scss";
+import { Header } from "./container/Header/Header";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Header />
     </div>
   );
 };
