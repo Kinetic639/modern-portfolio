2d494ad6fa17b69549b1b5b393559d9249f264af
Kinetic639 <michal.stepien36@gmail.com>
2023-04-29 15:03:04 +0200

    Implemented About section

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -3,12 +3,14 @@ import { Navbar } from "./components/Navbar/Navbar";
 
 import "./App.scss";
 import { Header } from "./container/Header/Header";
+import { About } from "./container/About/About";
 
 const App = () => {
   return (
     <div>
-      <Navbar />
-      <Header />
+      {/*<Navbar />*/}
+      <About />
+      {/*<Header />*/}
     </div>
   );
 };
