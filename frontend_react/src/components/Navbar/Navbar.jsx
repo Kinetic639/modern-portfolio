5e69481b1bb34ff32a0bcb71677263ca08422180
Kinetic639 <michal.stepien36@gmail.com>
2023-05-13 07:34:10 +0200

    Added style variables and fonts

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,51 +1,59 @@
-import { useState } from "react";
-import { HiMenuAlt4, HiX } from "react-icons/hi";
-import { motion } from "framer-motion";
+import {useContext, useState} from "react";
+import {HiMenuAlt4, HiX} from "react-icons/hi";
+import {motion} from "framer-motion";
 
-import { images } from "../../constants";
+import {images} from "../../constants";
 import "./Navbar.scss";
+import {ThemeContext} from "../../App";
 
 export const Navbar = () => {
-  const [toggle, setToggle] = useState(false);
+    const [toggle, setToggle] = useState(false);
+    const themeContext = useContext(ThemeContext);
+    const {theme, toggleTheme} = themeContext;
 
-  return (
-    <nav className="app__navbar">
-      <div className="app__navbar-logo">
-        <img src={images.logo} alt="logo" />
-      </div>
-      <ul className="app__navbar-links">
-        {["home", "about", "work", "skills", "contact"].map((page) => (
-          <li
-            className="app__navbar-link app__flex p-text"
-            key={`link-${page}`}
-          >
-            <div />
-            <a href={`#${page}`}>{page}</a>
-          </li>
-        ))}
-      </ul>
-
-      <div className="app__navbar-menu">
-        <HiMenuAlt4 onClick={() => setToggle(true)} />
-
-        {toggle && (
-          <motion.div
-            whileInView={{ x: [300, 0] }}
-            transition={{ duration: 0.85, ease: "easeOut" }}
-          >
-            <HiX onClick={() => setToggle(false)} />
-            <ul>
-              {["home", "about", "work", "skills", "contact"].map((item) => (
-                <li key={item}>
-                  <a href={`#${item}`} onClick={() => setToggle(false)}>
-                    {item}
-                  </a>
-                </li>
-              ))}
+    return (
+        <nav className="app__navbar">
+            <div className="app__navbar-logo">
+                <img src={images.logo} alt="logo"/>
+            </div>
+            <ul className="app__navbar-links">
+                {["home", "about", "work", "skills", "contact"].map((page) => (
+                    <li
+                        className="app__navbar-link app__flex p-text"
+                        key={`link-${page}`}
+                    >
+                        <div/>
+                        <a href={`#${page}`}>{page}</a>
+                    </li>
+                ))}
             </ul>
-          </motion.div>
-        )}
-      </div>
-    </nav>
-  );
+
+            <div className="app__navbar-menu">
+                <HiMenuAlt4 onClick={() => setToggle(true)}/>
+
+                {toggle && (
+                    <motion.div
+                        whileInView={{x: [300, 0]}}
+                        transition={{duration: 0.85, ease: "easeOut"}}
+                    >
+                        <HiX onClick={() => setToggle(false)}/>
+                        <ul>
+                            {["home", "about", "work", "skills", "contact"].map((item) => (
+
+                                <li key={item}>
+                                    <a href={`#${item}`} onClick={() => setToggle(false)}>
+                                        {item}
+                                    </a>
+                                </li>
+
+                            ))}
+                            <li key="button">
+                                <button onClick={toggleTheme}>{theme}</button>
+                            </li>
+                        </ul>
+                    </motion.div>
+                )}
+            </div>
+        </nav>
+    );
 };
