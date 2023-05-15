3cabc88e2657a0e6b4c8308a4920e0a2914d76b4
Kinetic639 <michal.stepien36@gmail.com>
2023-05-15 13:41:21 +0200

    Added tooltips to nav and settings items

--- frontend_react/src/components/Settings/ThemeSelect/ThemeSelect.jsx
+++ frontend_react/src/components/Settings/ThemeSelect/ThemeSelect.jsx
@@ -3,6 +3,7 @@ import "./ThemeSelect.scss";
 import {AppContext} from "../../../App";
 import {CgSun} from "react-icons/cg"
 import {FaMoon} from "react-icons/fa"
+import {Tooltip} from "react-tooltip";
 
 export const ThemeSelect = () => {
     const themeContext = useContext(AppContext);
@@ -11,10 +12,14 @@ export const ThemeSelect = () => {
     const changeTheme = (label) => {
         toggleTheme()
     };
-    return (
-        <button className="settings-button settings-button--theme" onClick={changeTheme}>
-            {theme === 'light' ? <CgSun/> :
-                <FaMoon/>}
-        </button>
+    return (<>
+            <button className="settings-button settings-button--theme" onClick={changeTheme}>
+                {theme === 'light' ? <CgSun/> :
+                    <FaMoon/>}
+                <Tooltip className='example' anchorSelect=".settings-button--theme" place="top">
+                    zmień widok
+                </Tooltip>
+            </button>
+        </>
     );
 };
